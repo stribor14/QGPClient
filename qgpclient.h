6aa16a646bcccb276e71797eb9adcf4dcae50a64
stribor14 <mirkokot@gmail.com>
2018-05-02 09:28:48 +0200

    Add MIT license header

--- qgpclient.h
+++ qgpclient.h
@@ -1,3 +1,37 @@
+/************************************************************************************
+**                                                                                 **
+**  QGPClient: simple TCP communication client for Gazepoint GP3 HD gaze           **
+**  tracker for use within Qt framework.                                           **
+**                                                                                 **
+**  MIT License                                                                    **
+**                                                                                 **
+** Copyright (c) 2018 stribor14                                                    **
+**                                                                                 **
+** Permission is hereby granted, free of charge, to any person obtaining a copy    **
+** of this software and associated documentation files (the "Software"), to deal   **
+** in the Software without restriction, including without limitation the rights    **
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       **
+** copies of the Software, and to permit persons to whom the Software is           **
+** furnished to do so, subject to the following conditions:                        **
+**                                                                                 **
+** The above copyright notice and this permission notice shall be included in all  **
+** copies or substantial portions of the Software.                                 **
+**                                                                                 **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      **
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        **
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     **
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          **
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   **
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   **
+** SOFTWARE.                                                                       **
+**                                                                                 **
+*************************************************************************************
+**           Author: Mirko Kokot                                                   **
+**          Contact: mirko.kokot@fer.hr                                            **
+**             Date: 29.04.18                                                      **
+**          Version: 1.0                                                           **
+*************************************************************************************/
+
 #ifndef QGPCLIENT_H
 #define QGPCLIENT_H
 
